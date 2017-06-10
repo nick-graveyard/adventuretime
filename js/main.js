1facde57e44bd373f99547b7d11e398735cfc3ae
Nick T-bone Kiermaier <nickkiermaier@gmail.com>
2017-06-11 00:41:25 +0100

    upd

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    $('#panel-1-click').click(function(){
+        $('#panel-1').css( "visibility", "hidden" );
+        $('#panel-2').css("visibility", "visible");
+    });
+
+
+
+});
